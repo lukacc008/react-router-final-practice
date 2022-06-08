11559f550e61ddf2f0f69726a551a115f16f127b
unknown <lukacc008@gmail.com>
2022-06-08 16:59:16 +0200

    Adding nested dynamic route

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,5 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
 import { Fragment } from "react";
+
+import Comments from "../comments/Comments";
 
 const QuoteDeatil = () => {
   const params = useParams();
@@ -8,6 +10,9 @@ const QuoteDeatil = () => {
     <Fragment>
       <h1>Quote Details!</h1>
       <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
