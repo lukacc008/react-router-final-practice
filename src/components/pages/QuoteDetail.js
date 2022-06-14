48f89e0983b5279cdf22d6b9fba5fec6deb490ec
unknown <lukacc008@gmail.com>
2022-06-14 15:09:13 +0200

    More flexible routing code. useHistory useLocation useRouteMatch hooks

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,30 +1,35 @@
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import { Fragment } from "react";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 
 import Comments from "../comments/Comments";
 
 const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Max', text: 'Learning react is fun!'},
-  {id: 'q2', author: 'Luka', text: 'Learning react is great!'},
+  { id: "q1", author: "Max", text: "Learning react is fun!" },
+  { id: "q2", author: "Luka", text: "Learning react is great!" },
 ];
 
 const QuoteDeatil = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No Quote Found!</p>
+    return <p>No Quote Found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <div className="centered">
-        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>Load Comments</Link>
-      </div>
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation, useRouteMatch } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
@@ -25,7 +25,10 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortinghandler = () => {
-    history.push("/quotes?sort=" + (isSortingAscending ? 'desc' : 'asc'));
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+    });
   };
 
   return (
